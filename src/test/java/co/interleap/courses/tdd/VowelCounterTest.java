91ee673c9f0134093b44b86b6fcf885bafdc6d2a
Leydis Garzón Giro <leydis.garzon@gmail.com>
2020-09-25 17:28:53 -0500

    Initial test - empty string

--- src/test/java/co/interleap/courses/tdd/VowelCounterTest.java
+++ src/test/java/co/interleap/courses/tdd/VowelCounterTest.java
@@ -1,9 +1,15 @@
 package co.interleap.courses.tdd;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class VowelCounterTest {
 
     @Test
-    public void dummy(){}
+    public void counterZeroForEmptyString(){
+        VowelCounter vowelCounter = new VowelCounter();
+        int count = vowelCounter.count("");
+        Assert.assertEquals(0, count);
+    }
+
 }
